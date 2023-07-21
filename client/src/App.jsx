078a55f424c791cbff2eb694ecd1e29274ef0ca6
Ayush <unitedestates.ayush10@gmail.com>
2023-07-21 18:27:09 +0530

    second commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-[20px] text-blue-600 tex">hello react + vite app</h1>
+      <h2 className="">asfaf</h2>
+    </>
+  );
+}
+
+export default App;
