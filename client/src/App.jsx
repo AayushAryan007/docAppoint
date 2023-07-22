ecdd1d1c6aee193060e09db2228c227094874cc5
Ayush <unitedestates.ayush10@gmail.com>
2023-07-22 14:31:19 +0530

    layout folders with assets

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,7 @@
-import { useState } from "react";
-
+import "./App.css";
+import Layout from "./layout/Layout";
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <h1 className="text-[20px] text-blue-600 tex">hello react + vite app</h1>
-      <h2 className="">asfaf</h2>
-    </>
-  );
+  return <Layout />;
 }
 
 export default App;
