4279b1d635ea4c0e71a8d952e8c1e739ed248aa8
Ayush <unitedestates.ayush10@gmail.com>
2023-07-26 23:10:08 +0530

    extra

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -86,7 +86,7 @@ const Header = () => {
                 </figure>
               </Link>
             </div>
-
+            {/* login button and link */}
             <Link to="/login">
               <button className="bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center justify-center rounded-[50px]">
                 Login
