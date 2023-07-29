9081c37393545ff6a7592181f2270e3293069eca
Ayush <unitedestates.ayush10@gmail.com>
2023-07-29 20:39:10 +0530

    hero started

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,45 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      {/* ===========hero section========== */}
+      <>
+        <section className="hero__section pt-[60px] 2xl:h-[800px]">
+          <div className="container">
+            <div className="flex flex-grow lg:flex-row gap-[90px] items-center justify-between">
+              {/* ========hero content=========== */}
+              <div>
+                <div className="lg:w-[570px]">
+                  <h1 className="text-[36px] leading-[46px] text-headingColor font-[800] md:text-[60px] md:leading-[70px]">
+                    We help patients live a healthy, longer life.
+                  </h1>
+                  <p className="text__para">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam
+                    provident voluptate quaerat placeat eveniet eum quas ipsum,
+                    consequatur nobis optio harum voluptatum labore officiis
+                    ktgj lk jwlkemt;lm .
+                  </p>
+
+                  <button className="btn">Request and Appointment</button>
+                </div>
+                {/* ======hero content====== */}
+                <div className="mt-[30px] lg:mt-[70px] flex flex-col lg:flex-row lg:items-center gap-5 lg_gap-[30px]">
+                  <div>
+                    <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
+                      30+
+                    </h2>
+                    <span className="w-[100px] h-2 bg-yellowColor rounded-full block mt-[14px]"></span>
+                    <p>Years of Experience</p>
+                  </div>
+                </div>{" "}
+              </div>
+            </div>
+          </div>
+        </section>
+      </>
+    </>
+  );
 };
 
 export default Home;
