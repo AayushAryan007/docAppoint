ead0d2dc3558267b394413eac22e1cfd4fac2a85
Ayush <unitedestates.ayush10@gmail.com>
2023-08-01 00:37:50 +0530

    feature image

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -186,6 +186,29 @@ const Home = () => {
         <div className="container">
           <div className="flex items-center justify-between flex-col lg:flex-row">
             {/* ====feature content===== */}
+            <div className="xl:w-[670px]">
+              <h2 className="heading">
+                get virtual treatment <br /> anytime.
+              </h2>
+
+              <ul className="pl-4">
+                <li className="text__para">
+                  1. Schedule the appointment directly.
+                </li>
+                <li className="text__para">
+                  2. Search for your physician here, and contact their office.
+                </li>
+
+                <li className="text__para">
+                  3. View your physicians who are accepting new patients, use
+                  the online scheduling tool to select an appointment time.
+                </li>
+              </ul>
+
+              <Link to="/">
+                <button className="btn">Learn More</button>
+              </Link>
+            </div>
           </div>
         </div>
       </section>
