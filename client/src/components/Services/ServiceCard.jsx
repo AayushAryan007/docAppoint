ee95655682fee30ec5080ab39d60b109262c7076
Ayush <unitedestates.ayush10@gmail.com>
2023-07-31 22:20:08 +0530

    service section error fised

--- client/src/components/Services/ServiceCard.jsx
+++ client/src/components/Services/ServiceCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const ServiceCard = ({ items, index }) => {
-  const { name, desc, bgColor, textColor } = items;
+const ServiceCard = ({ item, index }) => {
+  const { name, desc, bgColor, textColor } = item;
   return (
     <div className="py-[30px] px-3 lg:px-5">
       <h2 className="text-[26px] leading-9 text-headingColor font-[700]">
