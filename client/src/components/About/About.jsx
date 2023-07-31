61130d0e6c67bbec23e74210888f263f7623e05d
Ayush <unitedestates.ayush10@gmail.com>
2023-07-31 15:46:42 +0530

    about sections complete

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import aboutImg from "../../assets/images/about.png";
 import aboutCardImg from "../../assets/images/about-card.png";
 
@@ -18,6 +19,23 @@ const About = () => {
           {/* ======about content======== */}
           <div className="w-full lg:w-1/2 xl:w-[670px] order-1 lg:order-2">
             <h2 className="heading">Proud to be one of the nation's best</h2>
+            <p className="text__para">
+              For 30 years in row, U.S. News & World Report has recognized us as
+              one of the best publics hospitals in the Nation and #1 in Texas.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia.
+            </p>
+
+            <p className="text__para mt-[30px]">
+              Our best is something we strive for each day,caring for our
+              patients-not looking back at what we accomplished but towards what
+              we can do tomorrow. Providing the best. Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Incidunt magnam officiis recusandae
+              dignissimos.
+            </p>
+
+            <Link to="/">
+              <button className="btn">Learn More</button>
+            </Link>
           </div>
         </div>
       </div>
