9320ec6e8c22280f7b842573e865d07c618aabde
Ayush <unitedestates.ayush10@gmail.com>
2023-08-04 03:29:30 +0530

    faq questions

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -10,9 +10,11 @@ import { BsArrowRight } from "react-icons/bs";
 import About from "../components/About/About";
 import ServiceList from "../components/Services/ServiceList";
 import featureImg from "../assets/images/feature-img.png";
+import faqImg from "../assets/images/faq-img.png";
 import videoIcon from "../assets/images/video-icon.png";
 import avatarIcon from "../assets/images/avatar-icon.png";
 import DoctorList from "../components/Doctors/DoctorList";
+import FaqList from "../components/Faq/FaqList";
 
 const Home = () => {
   return (
@@ -266,7 +268,18 @@ const Home = () => {
       {/* ============faq Section============== */}
       <section>
         <div className="container">
-          <div className="flex justify-between gap-[50px] lg:gap-0"></div>
+          <div className="flex justify-between gap-[50px] lg:gap-0">
+            <div className="w-1/2 hidden md:block">
+              <img src={faqImg} alt="" />
+            </div>
+
+            <div className="w-full md:w-1/2">
+              <h2 className="heading">
+                Most Questions by our beloved patients
+              </h2>
+              <FaqList />
+            </div>
+          </div>
         </div>
       </section>
       {/* ============faq Section ends============== */}
